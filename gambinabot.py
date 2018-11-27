a8e771054136e6fd1430b6e6bcbe5b58ea1bc31d
Mikko Rouru <mikkorouru@Mikko-Mac-mini.local>
2018-11-27 10:05:09 +0200

    Removing the infinite loop from the bot

--- gambinabot.py
+++ gambinabot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import tweepy, urllib2, random, pickle, time
+import tweepy, urllib2, random, pickle
 from bs4 import BeautifulSoup
 
 try:
@@ -21,7 +21,7 @@ try:
 	soup = BeautifulSoup(page, 'html.parser')
 	last48Stores = []
 	stores = []
-	
+
 	gambinaRequest = urllib2.urlopen(gambinaPage)
 	gambinaSoup = BeautifulSoup(gambinaRequest, 'html.parser')
 
@@ -30,45 +30,40 @@ try:
 	stores = map(lambda s: s.strip(), stores)	
 	
 	#Main function for handling the stores, amount of Gambina and forming the tweet
-	while True:
-		def main():
-			pickle_file = open('gambinafile.pickle', 'ab')
-			amountOfGambinas = []
+	def main():
+		pickle_file = open('gambinafile.pickle', 'ab')
+		amountOfGambinas = []
 
-			try:
-				last48Stores = pickle.load(open('gambinafile.pickle', 'rb'))
-			except EOFError:
-				last48Stores = []
+		try:
+			last48Stores = pickle.load(open('gambinafile.pickle', 'rb'))
+		except EOFError:
+			last48Stores = []
 
+		randomAlko = (random.randint(1, len(stores)-1))
+		while randomAlko in last48Stores:
 			randomAlko = (random.randint(1, len(stores)-1))
-			while randomAlko in last48Stores:
-				randomAlko = (random.randint(1, len(stores)-1))
 
-			if len(last48Stores) < 48:
-				last48Stores.append(randomAlko)
-				with open ('gambinafile.pickle', 'wb') as pickle_file:
-					pickle.dump(last48Stores, pickle_file)
-			else:
-				last48Stores.remove(last48Stores[0])
-				last48Stores.append(randomAlko)
-				with open ('gambinafile.pickle', 'wb') as pickle_file:
-					pickle.dump(last48Stores, pickle_file)
+		if len(last48Stores) < 48:
+			last48Stores.append(randomAlko)
+			with open ('gambinafile.pickle', 'wb') as pickle_file:
+				pickle.dump(last48Stores, pickle_file)
+		else:
+			last48Stores.remove(last48Stores[0])
+			last48Stores.append(randomAlko)
+			with open ('gambinafile.pickle', 'wb') as pickle_file:
+				pickle.dump(last48Stores, pickle_file)
 
-			for selectedStoreGambinaAmount in gambinaSoup.find_all('span',  {"class": "right tiny-2 number-in-stock padding-h-0 taste-color "}):
-				amountOfGambinas.append(selectedStoreGambinaAmount.getText())
+		for selectedStoreGambinaAmount in gambinaSoup.find_all('span',  {"class": "right tiny-2 number-in-stock padding-h-0 taste-color "}):
+			amountOfGambinas.append(selectedStoreGambinaAmount.getText())
 
-			amountOfGambinaInSelectedStore = amountOfGambinas[randomAlko]
-			storeString = u'myymälässä '
-			tweet = "Gambinaa on saatavilla " + amountOfGambinaInSelectedStore + " pulloa " + storeString + stores[randomAlko]
-			print(last48Stores)
-			print(stores[randomAlko] + str(randomAlko))
-			print(tweet)
-			time.sleep(2)
+		amountOfGambinaInSelectedStore = amountOfGambinas[randomAlko]
+		storeString = u'myymälässä '
+		tweet = "Gambinaa on saatavilla " + amountOfGambinaInSelectedStore + " pulloa " + storeString + stores[randomAlko]
 
-			#api.update_status(tweet)
+		api.update_status(tweet)
 
-		if __name__ == "__main__":
-			main()
+	if __name__ == "__main__":
+		main()
 
 except Exception as e:
     print(e.message)
